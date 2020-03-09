928571b5dc5994af465466f39a0c3e09903f9418
karoliszagarnauskas <35958904+karoliszagarnauskas@users.noreply.github.com>
2020-03-09 08:28:14 +0200

    Add files via upload

---
+++ js-debuger-klaidu-ieskojimas/master.js
@@ -0,0 +1,21 @@
+console.log(" as veikiu");
+//kaip greiciau rasti klaias
+// 1) naudoti console.log("veikia - isvedimas")
+for (var i = 0; i < 6; i++) {
+  console.log(" ciklas for");  //test
+}
+//2) testuoti kuo mazesnes kodo dalis
+//pvz: viena f-ja, viena for
+
+// testuoti su DEBUGER (ilgiau uztrunka, bet patikrina visus zingsnius)
+// kintamuju reiksmes, if ar true ar false, parodyti FOR ciklas
+
+let x;   //undefined
+ x= 10;
+ x= 20;
+if (x<1000) {
+
+ for (var i = 0; i < 6; i++) {
+   console.log(" ciklas for");
+ }
+}
